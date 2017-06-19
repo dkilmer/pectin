b6bf0e03837eb32a88d70c2d44eb5c6d1356cd5b
David Kilmer <dkilmer@gmail.com>
2017-06-18 20:24:05 -0400

    line render example

--- window.c
+++ window.c
@@ -17,7 +17,7 @@ bool init_window(const char *program_name, int w, int h)
 	// SDL_GL_CONTEXT_CORE gives us only the newer version, deprecated functions are disabled
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-	// 3.2 is part of the modern versions of OpenGL, but most video cards whould be able to run it
+	// 3.2 is part of the modern versions of OpenGL, but most video cards whould be able to run_trifle it
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
